3e1e9175fc7868914e15a5282cdd1c33c8fbc03c
Christian Kraemer <christian.kraemer@tngtech.com>
2016-08-24 22:05:54 +0200

    imported upload

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-nodemcu-uploader --baud 115200 --start_baud 9600 upload *.lua
-nodemcu-uploader --baud 115200 --start_baud 9600 upload index.min.html
+nodemcu-uploader --baud 9600 --start_baud 9600 upload *.lua
+html-minifier --html5 --remove-optional-tags --collapse-whitespace --minify-css true --minify-js true index.html  > index.min.html
+nodemcu-uploader --baud 9600 --start_baud 9600 upload index.min.html
