988c99d4d230af29f00685dfc77496c90fa007cb
Christian Kraemer <christian.kraemer@tngtech.com>
2016-12-21 10:17:05 +0100

    do not minify if minfier is n/a

--- upload.sh
+++ upload.sh
@@ -3,8 +3,8 @@ sleep 1
 nodemcu-uploader --baud 9600 --start_baud 9600 file remove init.lua
 nodemcu-uploader --baud 9600 --start_baud 9600 node restart
 sleep 1
-nodemcu-uploader --baud 9600 --start_baud 9600 upload -c server.lua connection.lua adxl.lua motor.lua config.lua
+nodemcu-uploader --baud 9600 --start_baud 9600 upload -c server.lua connection.lua adxl.lua motor.lua config.lua keys.lua
 nodemcu-uploader --baud 9600 --start_baud 9600 upload *.lua
-html-minifier --html5 --remove-optional-tags --collapse-whitespace --minify-css true --minify-js true index.html  > index.min.html
+html-minifier --html5 --remove-optional-tags --collapse-whitespace --minify-css true --minify-js true index.html  > index.min.html || cp index.html index.min.html
 nodemcu-uploader --baud 9600 --start_baud 9600 upload index.min.html
 nodemcu-uploader --baud 9600 --start_baud 9600 node restart
